e4ccf258907d0bbea527e36d15aa54dd001042c4
Myles Borins <myles@famo.us>
2015-02-02 13:34:40 -0800

    Finish writing tests for standalone

--- test/test-standalone.js
+++ test/test-standalone.js
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 'use strict';
 
 var path = require('path');
@@ -19,10 +18,10 @@ var standalone = require('../lib/standalone');
 
 var famousSRC = path.join(__dirname, '..', 'node_modules', 'famous', 'src');
 
-test('standalone: setup', function (t) {
+test('standalone: setup temp directory', function (t) {
   t.plan(1);
   mkdirp(outDir, function (err) {
-    t.error(err, 'the folder should be made without an error');
+    t.error(err, 'the output folder should be made without an error');
   });
 });
 
@@ -33,13 +32,34 @@ test('standalone: exports', function (t) {
   });
 });
 
-// test('standalone: can be required', function (t) {
-//   t.plan(1);
-//   var famous = require(outFile);
-//   t.ok(famous);
-// });
+test('standalone: setup virtualdom', function (t) {
+  var jsdom = require('jsdom').jsdom;
+  var document = global.document = jsdom('hello famo.us');
+  var window = global.window = document.parentWindow;
+  var navigator = global.navigator = require('navigator');
+  var raf = global.window.requestAnimationFrame = require('raf');
+  t.plan(3);
+  t.ok(window, 'the virtual dom is set up');
+  t.ok(navigator, 'we have shimmed navigator');
+  t.ok(raf, 'we have shimmed requestAnimationFrame');
+});
 
-test('standalone: cleanup', function (t) {
+test('standalone: can be required', function (t) {
+  t.plan(5);
+  var famous = global.famous = require(outFile);
+  var core = famous.core;
+  var Engine = core.Engine;
+  t.ok(famous, 'the module is required as expected');
+  t.ok(core, 'the module has a core object');
+  t.ok(Engine, 'the module exposes the engine');
+  t.equal(Engine.getOptions('runLoop'), true, 'the engine should be running');
+  Engine.setOptions({
+    runLoop: false
+  });
+  t.equal(Engine.getOptions('runLoop'), false, 'the engine should no longer be running after being turned off');
+});
+
+test('standalone: teardown', function (t) {
   t.plan(1);
   rimraf(outDir, function (err) {
     t.error(err, 'the folder should be removed without an error');
