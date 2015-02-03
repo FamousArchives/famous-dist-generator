619ccd08912ccb6fa77a4b27e8e1edf0f4e67211
Myles Borins <myles@famo.us>
2015-02-03 12:53:57 -0800

    Test minification on amd

--- test/amd-test.js
+++ test/amd-test.js
@@ -5,6 +5,7 @@
 
 var path = require('path');
 var os = require('os');
+var fs = require('fs');
 
 var test = require('tape');
 var mkdirp = require('mkdirp');
@@ -18,6 +19,7 @@ var amd = require('../lib/amd');
 var tmpdir = os.tmpdir();
 var outDir = path.join(tmpdir, 'famous-dist-generator', 'amd-' + Date.now());
 var outFile = path.join(outDir, 'famous.js');
+var outFileMin = path.join(outDir, 'famous.min.js');
 
 var famousSRC = path.join(__dirname, '..', 'node_modules', 'famous', 'src');
 
@@ -29,9 +31,20 @@ test('amd: setup temp directory', function (t) {
 });
 
 test('amd: build', function (t) {
-  t.plan(1);
+  t.plan(2);
   amd(famousSRC, outFile, function (err) {
     t.error(err, 'the process should complete without an error');
+    t.ok(fs.existsSync(outFile), 'famous.js should exist');
+  });
+});
+
+test('amd: build minified', function (t) {
+  t.plan(2);
+  amd(famousSRC, outFileMin, true, function (err) {
+    var big = fs.readFileSync(outFile, 'utf8');
+    var small = fs.readFileSync(outFileMin, 'utf8');
+    t.error(err, 'the process should complete without an error');
+    t.ok(small.length < big.length, 'the minified version should be smaller');
   });
 });
 
