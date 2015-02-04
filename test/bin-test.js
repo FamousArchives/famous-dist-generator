d34775f1fa6c5b1178f85c5b2128f66c7b92802b
Myles Borins <myles@famo.us>
2015-02-04 00:33:15 -0800

    Feature parity

--- test/bin-test.js
+++ test/bin-test.js
@@ -2,44 +2,94 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 'use strict';
+var path = require('path');
+var os = require('os');
+var exec = require('child_process').exec;
 
 var test = require('tape');
-// var spawn = require('child_process').spawn;
-// var exec = require('child_process').exec;
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
+var src = path.join(__dirname, '..', 'node_modules', 'famous', 'src');
+var binPath = path.join(__dirname, '..', 'bin', 'cmd.js');
+var outDir = path.join(os.tmpdir(), 'famous-dist-generator', 'bin-' + Date.now());
+
+test('bin: setup temp directory', function (t) {
+  t.plan(1);
+  mkdirp(outDir, function (err) {
+    t.error(err, 'the output folder should be made without an error');
+  });
+});
 
 test('bin: build standalone/global', function (t) {
-  t.ok(true, 'shim');
-  t.end();
+  t.plan(3);
+  var dest = path.join(outDir, 'famous-standalone.js');
+  exec([binPath, src, dest].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'Successfully built Global famous\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
 });
 
-test('bin: build RequireJS version', function (t) {
-  t.ok(true, 'shim');
-  t.end();
-});
-
-test('bin: copy famous.css for specific version', function (t) {
-  t.ok(true, 'shim');
-  t.end();
+test('bin: build standalone/global minified', function (t) {
+  t.plan(3);
+  var dest = path.join(outDir, 'famous-standalone.min.js');
+  exec([binPath, src, dest, '-m'].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'Successfully built Global famous\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
 });
 
 test('bin: build CommonJS version', function (t) {
-  t.ok(true, 'shim');
-  t.end();
-  // var expectedTopLevelFiles = require('./top-level-commonjs-files-and-folders.json');
-  // t.plan(3 + expectedTopLevelFiles.length);
-  // var version = '0.2.1';
-  // var outPath = temp.path({prefix: 'famous-' + version});
-  // exec([binPath, '--commonjs', '--out', outPath, '--version', version].join(' '), function(err, stdout, stderr) {
-  //   t.error(err, 'No error returned.');
-  //   t.equal(stderr, '', 'stderr should be empty string');
-  //   var successMsg = 'Successfully built CommonJS famous for reference ' + version + '\n';
-  //   t.equal(stdout, successMsg, 'Printed success message.');
-  //   async.each(expectedTopLevelFiles, function(file, done) {
-  //     var filePath = path.join(outPath, file);
-  //     fs.exists(filePath, function(exists) {
-  //       t.ok(exists, 'Top level file exists: ' + file);
-  //       done();
-  //     });
-  //   }, temp.cleanupSync);
-  // });
+  t.plan(3);
+  var dest = path.join(outDir, 'common');
+  exec([binPath, src, dest, '--common'].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'Successfully built CommonJS famous\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
+});
+
+test('bin: build AMD version', function (t) {
+  t.plan(3);
+  var dest = path.join(outDir, 'famous.js');
+  exec([binPath, src, dest, '--amd'].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'Successfully built AMD famous\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
+});
+
+test('bin: build AMD version minified', function (t) {
+  t.plan(3);
+  var dest = path.join(outDir, 'famous.js');
+  exec([binPath, src, dest, '--amd', '--minify'].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'Successfully built AMD famous\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
+});
+
+test('bin: copy famous.css for specific version', function (t) {
+  t.plan(3);
+  var dest = path.join(outDir);
+  exec([binPath, src, dest, '--css'].join(' '), function (err, stdout, stderr) {
+    t.error(err, 'No error returned.');
+    t.equal(stderr, '', 'stderr should be empty string');
+    var successMsg = 'All Done Copying Famo.us CSS\n';
+    t.equal(stdout, successMsg, 'Printed success message.');
+  });
+});
+
+test('bin: teardown', function (t) {
+  t.plan(1);
+  rimraf(outDir, function (err) {
+    t.error(err, 'the folder should be removed without an error');
+  });
 });
